d2c3e81c829ad51344fb483c9ffaedfec6d1aee8
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-03-27 15:06:52 +0600

    Update Decimal To Binary Converter.java

--- Decimal To Binary Converter.java
+++ Decimal To Binary Converter.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,29 +24,35 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        StringBuilder res = new StringBuilder();
+
         editText = findViewById(R.id.editText);
         result = findViewById(R.id.result);
         convert = findViewById(R.id.convert);
 
         convert.setOnClickListener(v -> {
+            StringBuilder res = new StringBuilder();
             String decimal = editText.getText().toString();
             if (decimal.isEmpty()) {
-                result.setText("Please enter a decimal number!");
+                result.setText("PLEASE ENTER A DECIMAL NUMBER!");
             } else {
                 int decimalNumber = Integer.parseInt(decimal);
-                String a="";
-                while(decimalNumber!=0){
-                    int x=decimalNumber%2;
-                    String y=Integer.toString(x);
-                    a=a+y;
-                    decimalNumber=decimalNumber/2;
+                String a = "";
+                if (decimalNumber == 0) {
+                    result.setText("0");
+                } else {
+                    while (decimalNumber != 0) {
+                        int x = decimalNumber % 2;
+                        String y = Integer.toString(x);
+                        a = a + y;
+                        decimalNumber = decimalNumber / 2;
+                    }
+                    res.append(a);
+                    res.reverse();
+                    result.setText(res);
                 }
-                res.append(a);
-                res.reverse();
-                result.setText(res);
+                Toast.makeText(MainActivity.this, "THANKS DEAR", Toast.LENGTH_SHORT).show();
             }
-                });
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
